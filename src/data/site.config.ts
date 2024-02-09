ef7624aac9414e16cd36d5a00b83655f44771422
pratamaizzat <pratama.izzat231298@gmail.com>
2024-02-09 17:18:18 +0700

    share to whatsap

--- src/data/site.config.ts
+++ src/data/site.config.ts
@@ -12,8 +12,8 @@ export const siteConfig: SiteConfig = {
 	author: 'Fleetsumo', // Site author
 	title: 'Fleetsumo', // Site title.
 	description: 'Fleet Management System Pertama di Indonesia', // Description to display in the meta tags
-	lang: 'en',
-	ogLocale: 'en',
-	shareMessage: 'Bagikan', // Message to share a post on social media
+	lang: 'en-GB',
+	ogLocale: 'en_GB',
+	shareMessage: 'Share this post', // Message to share a post on social media
 	paginationSize: 6 // Number of posts per page
 }
